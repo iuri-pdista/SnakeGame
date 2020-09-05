be3e558ced1dbea3250730204b789543a056584e
iuri-pdista <correaiuri86@gmail.com>
2020-09-05 02:31:37 -0300

    Finished MapRender function

--- main.cpp
+++ main.cpp
@@ -5,18 +5,53 @@
 #include <time.h>
 #include <term.h>
 
+int mapWidth = 40;
+int mapHeight = 20;
+
 typedef struct fruit {
 	int x;
 	int y;
 	int WasEaten;
 } Fruit;
 
-typedef struct character 
+typedef struct character {
 	int x;
 	int y;
 	int score;
 } Character;
 
-int main (){
+void RenderMap () {
+	int x = 0;
+	do {
+		for ( int y = 0; y < mapHeight; y++ ){
+			move( y, x );
+			if ( y == 0 || y == 19 ){
+				printw("#");
+				refresh();
+			}
+			else{
+				if( x == 0 || x == 39){
+					printw("#");
+					refresh();
+				}
+				else{
+					printw(" ");
+					refresh();
+				}
+			}
+		}
+		printw("\n");
+		x += 1;
+	}while(x < mapWidth);
+}
 
+int main (){
+	initscr();
+	noecho();
+	system("clear");
+	RenderMap();
+	while (1){
+		getch();
+	}
+	endwin();
 }
