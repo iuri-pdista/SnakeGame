d6d28606886fd8364ef8f8ed084e8ba8573e854c
iuri-pdista <correaiuri86@gmail.com>
2020-09-04 23:12:15 -0300

    Created fruit struct

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <stdlib.h> 
+#include <ncurses.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+#include <term.h>
+
+typedef struct fruit {
+	int x;
+	int y;
+	int WasEaten;
+} Fruit;
+
+typedef struct character {
+} Character;
+
+int main (){
+
+}
